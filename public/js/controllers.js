754fa2381131ac63765d799b0e647730fbb17dc1
Christopher Kim <christopherjkim@gmail.com>
2012-10-09 17:26:01 -0400

    AddUp button sends AddUp event, pushes user in queue. Receives callback

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-function MainCtrl($scope, $location, $rootScope) {
+function MainCtrl($scope, $location, $rootScope, socket) {
   $scope.path = function() {
     return $location.path();
   };
@@ -13,4 +13,47 @@ function MainCtrl($scope, $location, $rootScope) {
   $rootScope.$on('$routeChangeSuccess', function() {
     $scope.loading = false;
   });
+
+  // Adding up as a class
+  $scope.addUp = function() {
+
+    var classes = [];
+    var validClasses = ['class-scout','class-psoldier','class-rsoldier', 'class-medic', 'class-demoman'];
+    socket.emit('addUp', validClasses, function(response) {
+      if (typeof(response) !== 'Number') {
+        // Do something
+      }
+      alert('Queue position: ' + response);
+    }); // Need a callback for this
+
+    // if ($(this).hasClass('active')) {
+    //   $(this).button('reset').button('toggle');
+    //   return ss.rpc('app.removeFromQueue');
+    // }
+    // var classes = $('#classes-selected').sortable('toArray');
+    // if (!hasSteamInfo()) {
+    //   return jqueryalert('You must sign in to do that.');
+    // } else {
+    //   return ss.rpc('app.addUp', classes, function(success) {
+    //     if (success) {
+    //       $('#addup-button').button('complete').button('toggle');
+    //       // $('#addup-button').toggleClass('disabled').setAttribute('disabled');
+    //       return;
+    //     } else {
+    //       return jqueryalert('Something went wrong.');
+    //     }
+    //   });
+    // };
+  };
+
+  // Socket.io
+
+  socket.on('addUp', function(data) {
+    console.log(data);
+  });
+  // socket.emit('stats:subscribe', $routeParams.id);
+  // socket.on('stats:send', function (data) {
+  //   $scope.stats = data.stats;
+  //   $scope.playerMetaData = data.playerdata;
+  // });
 }
--- routes/socket.js
+++ routes/socket.js
@@ -8,6 +8,13 @@ var parseCookie = require('connect').utils.parseCookie;
 module.exports = function(app) {
   var io = app.io;
 
+  // State
+  var state = {
+    users: [],
+    newbiequeue: [],
+    coachqueue: []
+  };
+
   // Express session authorization
 
   app.io.set('authorization', function(data, fn) {
@@ -26,10 +33,77 @@ module.exports = function(app) {
 
   io.sockets.on('connection', function(socket) {
 
-    console.log(socket.handshake.session);
+    var user = socket.handshake.session.auth.steam.user;
+    console.log('A socket connected: ');
+    console.log(socket.handshake.session.auth);
 
-    console.log('A socket with sessionID ' + socket.handshake.session +
-        ' connected!');
+
+    // Queue Stuff
+
+    socket.on('addUp', function(classes, callback) {
+      // Validate inputted array of user's chosen classes
+      if (!(Array.isArray(classes) && classes.length > 0 && classes.length < 6)) { return res(false); }
+      var validClasses = ['class-scout','class-psoldier','class-rsoldier', 'class-medic', 'class-demoman'];
+      var addedUp = false;
+      // Create costs matrix for the Hungarian Algorithm
+      var costs = [100000,100000,100000,100000,100000,100000,100000,100000,100000,100000,100000,100000];
+      var prefscale = 1; // How much to weigh the order of class preferences
+      var duplicates = []; // Holds processed classes to check for duplicates
+      classes.forEach( function(classId, index) {
+        if (duplicates.indexOf(classId) > -1) {
+          return res(false);
+        }
+        if (classId === 'class-scout') {
+          costs[0] = (index + 1)*prefscale;
+          costs[1] = (index + 1)*prefscale;
+          costs[2] = (index + 1)*prefscale;
+          costs[3] = (index + 1)*prefscale;
+        } else if (classId === 'class-psoldier') {
+          costs[4] = (index + 1)*prefscale;
+          costs[5] = (index + 1)*prefscale;
+        } else if (classId === 'class-rsoldier') {
+          costs[6] = (index + 1)*prefscale;
+          costs[7] = (index + 1)*prefscale;
+        } else if (classId === 'class-demoman') {
+          costs[8] = (index + 1)*prefscale;
+          costs[9] = (index + 1)*prefscale;
+        } else if (classId === 'class-medic') {
+          costs[10] = (index + 1)*prefscale;
+          costs[11] = (index + 1)*prefscale;
+        } else {
+          return res(false);
+        }
+        duplicates.push(classId);
+      });
+
+      // Update user with new classes/costs arrays
+      user.classes = classes;
+      user.costs = costs;
+
+      // If user is already in a queue, remove from queue. Then add to queue.
+      // You should probable make a removeFromQueue() function or something
+      var queueType = user.rank + 'queue';
+      for (var i=0, len=state[queueType].length; i<len; ++i) {
+        if (state[queueType][i]._id === user._id) {
+          state[queueType].splice(i,1);
+          break;
+        }
+      }
+      state[queueType].push(user);
+
+      console.log(state);
+      io.sockets.emit('addUp', {
+        _id: user._id,
+        classes: user.classes,
+        name: user.name,
+        rank: user.rank,
+        status: user.status
+      });
+      
+      callback(state.length); // Place in line
+    });
+
+    // Sprite/Chat Stuff
 
     socket.on('message', function(data) {
       if (Date.now() - socket.lastMessageAt < 100) {
@@ -39,7 +113,8 @@ module.exports = function(app) {
       data.id = socket.id;
       return socket.json.broadcast.send(data);
     });
-    return socket.on('disconnect', function() {
+    
+    socket.on('disconnect', function() {
       return socket.json.broadcast.send({
         id: socket.id,
         disconnect: true
@@ -47,5 +122,9 @@ module.exports = function(app) {
     });
 
   });
+
+  // Helpers
+
+
 
 };
